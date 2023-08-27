43e3a0db24f887bec1899035f7a2b7217cdbc9d7
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-27 15:59:22 +0600

    changes

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,10 +3,14 @@
 import { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
+
   // states
   const [isOpen, setIsOpen] = useState(false);
 
@@ -38,7 +42,7 @@ const UserMenu = () => {
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
             <MenuItem onClick={() => {}} label="Login" />
-            <MenuItem onClick={() => {}} label="Sign up" />
+            <MenuItem onClick={registerModal.onOpen} label="Sign up" />
           </div>
         </div>
       )}
