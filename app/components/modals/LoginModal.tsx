8ff44f1986402f2a0675519f6cbd3adbe1581ff6
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-29 01:14:13 +0600

    login with credentials

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,7 +1,133 @@
 "use client";
 
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+import Button from "../Button";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+import Modal from "./Modal";
+
 const LoginModal = () => {
-  return <div>LoginModal</div>;
+  const router = useRouter();
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // states
+  const [isLoading, setIsLoading] = useState(false);
+
+  // handler to submit form
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    signIn("credentials", {
+      ...data,
+      redirect: false,
+    }).then((callback) => {
+      setIsLoading(false);
+      if (callback?.ok) {
+        toast.success("Logged in!");
+        router.refresh();
+        loginModal.onClose();
+      }
+      if (callback?.error) {
+        toast.error(callback.error);
+      }
+    });
+  };
+
+  // change the modal
+  const changeModal = () => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  };
+
+  // body content
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome Back!" subTitle="Log in to your account" />
+      <Input
+        disabled={isLoading}
+        type="email"
+        id="email"
+        label="Email"
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        disabled={isLoading}
+        type="password"
+        id="password"
+        label="Password"
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  // footer content
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex justify-center items-center gap-2">
+          <div>New to airbnb?</div>
+          <div
+            onClick={changeModal}
+            className="font-semibold text-rose-500 cursor-pointer hover:underline"
+          >
+            Register
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      title="Login"
+      actionLabel="Continue"
+      isOpen={loginModal.isOpen}
+      onClose={loginModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
 };
 
 export default LoginModal;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,12 +3,14 @@
 import { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 
+import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
 
   // states
@@ -41,7 +43,7 @@ const UserMenu = () => {
       {isOpen && (
         <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
-            <MenuItem onClick={() => {}} label="Login" />
+            <MenuItem onClick={loginModal.onOpen} label="Login" />
             <MenuItem onClick={registerModal.onOpen} label="Sign up" />
           </div>
         </div>
