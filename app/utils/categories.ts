3c9fa44366bf6f0df430ab453700399a454bac1f
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-30 12:28:18 +0600

    all categories

--- app/utils/categories.ts
+++ app/utils/categories.ts
@@ -1,6 +1,18 @@
-import { GiWindmill } from "react-icons/gi";
+import { BsSnow } from "react-icons/bs";
+import { FaSkiing } from "react-icons/fa";
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCactus,
+  GiCastle,
+  GiCaveEntrance,
+  GiForestCamp,
+  GiIsland,
+  GiWindmill,
+} from "react-icons/gi";
+import { IoDiamond } from "react-icons/io5";
 import { MdOutlineVilla } from "react-icons/md";
-import { TbBeach } from "react-icons/tb";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
 
 export const categories = [
   {
@@ -17,5 +29,65 @@ export const categories = [
     label: "Modern",
     icon: MdOutlineVilla,
     description: "This property is modern.",
+  },
+  {
+    label: "Countryside",
+    icon: TbMountain,
+    description: "This property is in thhe country side.",
+  },
+  {
+    label: "Pools",
+    icon: TbPool,
+    description: "This property has a pool.",
+  },
+  {
+    label: "Islands",
+    icon: GiIsland,
+    description: "This property is an island.",
+  },
+  {
+    label: "Lake",
+    icon: GiBoatFishing,
+    description: "This property is close to a lake.",
+  },
+  {
+    label: "Skiing",
+    icon: FaSkiing,
+    description: "This property has skiing activities.",
+  },
+  {
+    label: "Castles",
+    icon: GiCastle,
+    description: "This property is in a castle.",
+  },
+  {
+    label: "Camping",
+    icon: GiForestCamp,
+    description: "This property has camping activities.",
+  },
+  {
+    label: "Arctic",
+    icon: BsSnow,
+    description: "This property is in the snow.",
+  },
+  {
+    label: "Cave",
+    icon: GiCaveEntrance,
+    description: "This property is in a cave.",
+  },
+  {
+    label: "Desert",
+    icon: GiCactus,
+    description: "This property is in desert.",
+  },
+  {
+    label: "Barns",
+    icon: GiBarn,
+    description: "This property is in the barn.",
+  },
+  {
+    label: "Lux",
+    icon: IoDiamond,
+    description: "This property is luxurious.",
   },
 ];
