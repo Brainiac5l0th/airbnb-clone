bb6b27a353c88efe0e3327aeff10e3df055701e6
shawonTalukder <shawontalukder.study@gmail.com>
2023-08-30 15:01:27 +0600

    add scrollbar

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -7,17 +7,31 @@ import CategoryBox from "../CategoryBox";
 import Container from "../Container";
 
 const Categories = () => {
+  // hooks
   const params = useSearchParams();
 
+  // get category from the params
   const category = params?.get("category");
 
   const pathname = usePathname();
 
+  const isMainPage = pathname === "/";
+
+  if (!isMainPage) return null;
   return (
     <Container>
-      <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+      <div
+        className="pt-4 flex flex-row items-center justify-between overflow-x-auto scrollbar 
+        scrollbar-h-[3px]
+        scrollbar-rounded-lg scrollbar-thumb-rose-400"
+      >
         {categories?.map((el) => (
-          <CategoryBox key={el.label} label={el.label} icon={el.icon} />
+          <CategoryBox
+            key={el.label}
+            label={el.label}
+            icon={el.icon}
+            selected={category === el.label}
+          />
         ))}
       </div>
     </Container>
