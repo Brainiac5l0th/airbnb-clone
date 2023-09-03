90a19d2f9b4bcfd691e582774e0138125fa63945
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-03 13:32:58 +0600

    into one file export

---
+++ app/components/rent/index.ts
@@ -0,0 +1,16 @@
+import CategoryList from "./CategoryList";
+import Information from "./Information";
+
+import Location from "./Location";
+import PlaceDescription from "./PlaceDescription";
+import PlaceImage from "./PlaceImage";
+import PlacePrice from "./PlacePrice";
+
+export {
+  CategoryList,
+  Information,
+  Location,
+  PlaceDescription,
+  PlaceImage,
+  PlacePrice,
+};
