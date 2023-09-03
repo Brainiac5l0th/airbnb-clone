181a13952f5ea2065b5fcd3961b2e1e448b6dab1
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-04 00:21:02 +0600

    to show listings components in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
-const Home = () => {
-  return <main className="text-3xl">Hello world!</main>;
+import { Listing } from "@prisma/client";
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+
+const Home = async () => {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+  // if there is nothing in the listings
+  // show empty state
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+  return (
+    <Container>
+      <div
+        className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
+        xl:grid-cols-5
+        2xl: grid-cols-6
+        gap-8"
+      >
+        {listings.map((el: Listing) => (
+          <ListingCard key={el.id} data={el} currentUser={currentUser} />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
