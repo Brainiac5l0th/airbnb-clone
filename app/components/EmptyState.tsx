e2a19ba5302d203e5b5b45e20e7ad982d5f9cc6d
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-03 23:55:46 +0600

    Not found component

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Button from "./Button";
+import Heading from "./Heading";
+
+interface EmptyStateProps {
+  title?: string;
+  subtitle?: string;
+  showReset?: boolean;
+}
+const EmptyState: React.FC<EmptyStateProps> = ({
+  title = "No exact matches",
+  subtitle = "Try changing or removing some of your filters",
+  showReset,
+}) => {
+  const router = useRouter();
+  return (
+    <div className="h-[60h] flex flex-col items-center gap-2 justify-center">
+      <Heading title={title} subTitle={subtitle} center />
+      <Button
+        outline
+        label={"Remove all filters"}
+        onClick={() => router.push("/")}
+      />
+    </div>
+  );
+};
+
+export default EmptyState;
