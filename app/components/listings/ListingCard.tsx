474d9ed1c67c069246bb4e94b1e906f2dffe8967
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-04 17:58:39 +0600

    making listing card

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { format } from "date-fns";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+
+import useCountries from "@/app/hooks/useCountries";
+
+import { SafeUser } from "@/app/types";
+import { Listing, Reservation } from "@prisma/client";
+import HeartButton from "../HeartButton";
+
+interface ListingCardProps {
+  data: Listing;
+  reservation?: Reservation;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  currentUser,
+  actionId = "",
+  actionLabel,
+  disabled,
+  onAction,
+  reservation,
+}) => {
+  const router = useRouter();
+  const { getByValue } = useCountries();
+
+  const location = getByValue(data?.locationValue);
+
+  // handlers
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (disabled) return;
+
+      onAction?.(actionId);
+    },
+    [disabled, onAction, actionId]
+  );
+
+  // formatting price
+  const price = useMemo(() => {
+    // if there is reservation, return it's total price
+    if (reservation) return reservation.totalPrice;
+
+    // else return it from listing data
+    return data.price;
+  }, [data.price, reservation]);
+
+  // date formatting
+  const reservationDate = useMemo(() => {
+    if (!reservation) return null;
+
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "pp")} - ${format(end, "pp")}`;
+  }, [reservation]);
+
+  return (
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className="col-span-1 cursor-pointer group"
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+          <Image
+            fill
+            alt={data.title}
+            src={data.imageSrc}
+            className="object-cover h-full w-full group-hover:scale-110 transition"
+          />
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListingCard;
