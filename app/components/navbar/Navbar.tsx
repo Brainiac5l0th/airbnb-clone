e4ffa84f9c485a48dde34269c75c8d7e856683c0
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-07 12:38:31 +0600

    [bug] issue:: modal was showing behind the navbar.. no crossbar shown

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -13,7 +13,7 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
-    <div className="fixed w-full bg-white shadow-md z-50">
+    <div className="fixed w-full bg-white shadow-md z-[10]">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
