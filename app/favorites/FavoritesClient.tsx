dc7d9aa26acd2086910dbfa570412a30bc96b931
shawonTalukder <shawontalukder.study@gmail.com>
2023-09-11 00:56:06 +0600

    margin added

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -16,7 +16,7 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
   return (
     <Container>
       <Heading title="Favorites" subTitle="List of places you favorited!" />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {favoritesListing.map((el) => (
           <ListingCard
             key={el.id}
